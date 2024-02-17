8866eca55f401c063246cc82307a3d90e5fe6c43
Dimon8963 <pruimakdmytro@gmail.com>
2024-02-17 17:49:22 +0200

    change main page

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,9 +1,11 @@
+import React from 'react';
+
 function MainContent() {
     return (
-        <div>
-            <div className={"container"}>
-                <div className={"row"}>
-                    <div className={"col"}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <div className="container">
+                <div className="row">
+                    <div className="col text-center">
                         <h1>Main Content</h1>
                         <p>This is the main content</p>
                     </div>
