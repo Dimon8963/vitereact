07f0fc85554ef08f86b618df51c2ebf5dbc5585b
Dimon8963 <pruimakdmytro@gmail.com>
2024-02-20 16:10:06 +0200

    добавили коменти та видалення їх

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import MainContent from './components/MainContent.jsx';
 import Footer from './components/Footer.jsx';
 import Navigation from "./components/Navigation.jsx";
+import Welcome from './components/Welcome.jsx'
+import Field from './components/Field.jsx';
+
 function App() {
+
     return (
         <>
             <Navigation/>
+
+            <Welcome name={"Dima"} lastname={"Pryimak"}/>
+            <Field label={"Введіть текст"} placeholder={"..."}/>
+
             <MainContent/>
+
             <Footer/>
         </>
-    );
+    )
 }
 
 export default App;
