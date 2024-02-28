8dafebb5d712304e6590acd6fe1cf2ce8484d97b
Dimon8963 <pruimakdmytro@gmail.com>
2024-02-28 19:25:28 +0200

    створив сторінки для роутерів

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../main.css"; // Підключаємо CSS файл
+
+const About = () => {
+    return (
+        <div className="developer-info">
+            <p>Цей сайт був розроблений в межах навчання.</p>
+            <p>Викладач: Сурков К.Ю.</p>
+            <p>Студент-розробник сайта: Приймак Дмитро</p>
+        </div>
+    );
+}
+
+export default About;
