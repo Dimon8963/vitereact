1f120dafce4fe7b7af44fc675d8b0720488ef0ec
Dimon8963 <pruimakdmytro@gmail.com>
2024-02-28 19:26:27 +0200

    прибрав MainContent

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,10 @@
+import React from "react";
 import Welcome from "../Welcome.jsx";
 import Field from "../Field.jsx";
 import List from "../List.jsx";
 import Task from "../Task.jsx";
 import MainContent from "../MainContent.jsx";
-import React from "react";
+
 
 
 const Home = () => {
