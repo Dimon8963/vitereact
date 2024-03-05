3f1951c99d2d34e3d3e54e957c39df9d9c8007cb
Dimon8963 <pruimakdmytro@gmail.com>
2024-03-05 17:06:10 +0200

    добавив посилання для PokeAPI

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./components/pages/About.jsx";
 import Services from "./components/pages/Services.jsx";
 import TestAPI from "./components/TestAPI.jsx";
+import PokeAPI from "./components/PokeAPI.jsx";
 
 function App() {
     return (
         <>
             <BrowserRouter>
                 <Navigation />
-                <TestAPI />
+                {/*<TestAPI />*/}
                 <Routes>
                     <Route path="/" element={<Home />} /> {/*Головна сторінка*/}
                     <Route path="/contacts" element={<Contacts />} />
                     <Route path={"gallery"} element={<Gallery/>} />
                     <Route path="/about" element={<About/>} />
                     <Route path="/services" element={<Services />} />
+                    <Route path="/pokeapi" element={<PokeAPI />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </BrowserRouter>
