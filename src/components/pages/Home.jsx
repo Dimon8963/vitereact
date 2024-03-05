55377c284482584c8d4b9ba4e039c606cbc932b3
Dimon8963 <pruimakdmytro@gmail.com>
2024-03-05 17:06:31 +0200

    добавив кнопку-посилання для PokeAPI

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,22 +3,22 @@ import Welcome from "../Welcome.jsx";
 import Field from "../Field.jsx";
 import List from "../List.jsx";
 import Task from "../Task.jsx";
-import MainContent from "../MainContent.jsx";
+import "../../main.css"
 
 
 
 const Home = () => {
     return (
-        <>
+        <div className="container"> {/* Додайте контейнер */}
             <Welcome name={"Dima"} lastname={"Pryimak"}/>
             <Field label={"Введіть текст"} placeholder={"..."}/>
+            <a href="/pokeapi" className="pokeapi-button">PokeAPI</a>
             <List/>
             <Task/>
             {/*<MainContent/>*/}
 
-        </>
+        </div>
     )
-
 }
 
 export default Home;
