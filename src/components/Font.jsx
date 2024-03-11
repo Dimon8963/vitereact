f2d94f48b2cb576b839f88d8bd2f3852f92e817c
Dimon8963 <pruimakdmytro@gmail.com>
2024-03-11 14:42:36 +0200

    для зміни розміру шрифта

---
+++ src/components/Font.jsx
@@ -0,0 +1,24 @@
+// FontSizeContext.js
+import React, { createContext, useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+
+const FontSizeContext = createContext();
+
+export const useFontSize = () => useContext(FontSizeContext);
+
+export const FontSizeProvider = ({ children }) => {
+    const [fontSize, setFontSize] = useState(16);
+
+    const increaseFontSize = () => setFontSize(prevSize => prevSize + 1);
+    const decreaseFontSize = () => setFontSize(prevSize => prevSize - 1);
+
+    return (
+        <FontSizeContext.Provider value={{ fontSize, increaseFontSize, decreaseFontSize }}>
+            {children}
+        </FontSizeContext.Provider>
+    );
+};
+
+FontSizeProvider.propTypes = {
+    children: PropTypes.node.isRequired
+};
