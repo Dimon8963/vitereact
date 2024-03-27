db2ea9fcc3c51948248f6470afec4d02c2375ab0
Dimon8963 <pruimakdmytro@gmail.com>
2024-03-27 16:34:09 +0200

    зміна для firebase

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Определите базовый URL на основе переменной окружения
+const base = (() => {
+  switch (process.env.DEPLOY_ENV) {
+    case 'gh-pages':
+      return '/vite_react/'; // для GitHub Pages
+    case 'firebase':
+      return '/'; // для Firebase Hosting
+    default:
+      return '/'; // для локальной разработки
+  }
+})();
+
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/vitereact/' : '/',
+  base,
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 3000
   }
-})
+});
