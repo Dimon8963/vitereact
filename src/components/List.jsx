985c8e380a77b14fc3131d4763ab69ea99d5ccc9
Dimon8963 <pruimakdmytro@gmail.com>
2024-04-03 20:53:07 +0300

    добавлення лічильника кліків по сердечку

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -7,10 +7,17 @@ import ToggleOffIcon from '@mui/icons-material/ToggleOff';
 import ToggleOnIcon from '@mui/icons-material/ToggleOn';
 import Checkbox from '@mui/material/Checkbox';
 import Students from '../list.json'; // Імпорт даних студентів
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { useSelector, useDispatch } from 'react-redux';
+import { incrementHeartClicks, decrementHeartClicks } from '../redux/actions.jsx'; // для збільшення/зменшення кількості натискань на серце
 
+// Компонент для фільтрації за містом
 const CityFilter = ({ onChange }) => {
     const options = useMemo(() => {
+        // Отримання унікальних міст з даних студентів
         const uniqueCities = [...new Set(Students.map(item => item.city))];
+        // Створення списку опцій для вибору міста
         return uniqueCities.map(city => (
             <option key={city} value={city}>{city}</option>
         ));
@@ -28,20 +35,37 @@ CityFilter.propTypes = {
     onChange: PropTypes.func.isRequired
 };
 
+// Компонент для відображення інформації про студента
 const StudentComponent = ({ student, dragState, onDragStart, onDragOver, onDrop }) => {
     const [isDragging, setIsDragging] = useState(false); // Стан для відстеження перетягування
+    const [isFavorite, setIsFavorite] = useState(false); // Стан для відстеження обраного студента
+    const heartClicks = useSelector(state => state.heartClicks); // Отримання кількості натискань на серце зі стейту
+    const dispatch = useDispatch(); // Отримання функції диспетчера Redux
 
+    // Обробник події початку перетягування
     const handleDragStart = (e) => {
-        setIsDragging(true);
+        setIsDragging(true); // Встановлення стану перетягування
         onDragStart(student.id); // Виклик функції початку перетягування
         e.dataTransfer.effectAllowed = 'move';
     };
 
+    // Обробник події завершення перетягування
     const handleDragEnd = () => {
         setIsDragging(false); // Завершення перетягування
     };
 
-    let className = isDragging ? 'user dragging' : 'user'; // Додавання класу для перетягування
+    // Обробник кліку на іконці серця
+    const handleOnClick = () => {
+        if (isFavorite) {
+            dispatch(decrementHeartClicks()); // Зменшення кількості натискань на серце
+        } else {
+            dispatch(incrementHeartClicks()); // Збільшення кількості натискань на серце
+        }
+        setIsFavorite(!isFavorite); // Зміна стану обраного студента
+    };
+
+    // Клас для компонента студента з урахуванням перетягування
+    let className = isDragging ? 'user dragging' : 'user';
 
     return (
         <div
@@ -51,12 +75,16 @@ const StudentComponent = ({ student, dragState, onDragStart, onDragOver, onDrop 
             onDragEnd={handleDragEnd}
             onDragOver={onDragOver}
             onDrop={onDrop}
-            id={student.id} // Присвоєння ID елементу
+            id={student.id}
         >
             <p>{student.id}</p>
             <p>{student.name}</p>
             <p>{student.absences}</p>
             <p>{student.city}</p>
+            {/* Відображення іконки серця залежно від стану */}
+            <div onClick={handleOnClick}>
+                {isFavorite ? <FavoriteIcon/> : <FavoriteBorderIcon/>}
+            </div>
         </div>
     );
 };
@@ -74,8 +102,10 @@ StudentComponent.propTypes = {
     onDrop: PropTypes.func.isRequired
 };
 
+// Підключення мемоїзованого компонента студента
 const Student = React.memo(StudentComponent);
 
+// Головний компонент списку студентів
 const List = () => {
     const [selectedCity, setSelectedCity] = useState(''); // Стан для обраного міста
     const [sortDirection, setSortDirection] = useState(null); // Стан для напрямку сортування
@@ -84,26 +114,31 @@ const List = () => {
     const [overId, setOverId] = useState(null); // Стан для ID над яким проводиться перетягування
     const [students, setStudents] = useState(Students); // Стан для даних студентів
 
+    // Обробник події початку перетягування
     const handleDragStart = (id) => {
         setDraggedId(id); // Встановлення ID перетягуваного елемента
     };
 
+    // Обробник події завершення перетягування
     const handleDragEnd = () => {
         setDraggedId(null);
         setOverId(null); // Скидання ID перетягування та над яким елементом перетягують
     };
 
+    // Обробник події перетягування над елементом
     const handleDragOver = (e) => {
         e.preventDefault();
         setOverId(parseInt(e.currentTarget.id, 10)); // Встановлення ID елементу над яким проводиться перетягування
     };
 
+    // Обробник події відпускання елемента
     const handleDrop = () => {
         if (draggedId && overId && draggedId !== overId) {
             setStudents(prevStudents => replaceDraggedAndOverItems(prevStudents, draggedId, parseInt(overId, 10))); // Перестановка елементів
         }
     };
 
+    // Функція для перестановки елементів при перетягуванні
     const replaceDraggedAndOverItems = (students, draggedId, overId) => {
         const indexDragged = students.findIndex(student => student.id === draggedId);
         const indexOver = students.findIndex(student => student.id === overId);
@@ -114,23 +149,28 @@ const List = () => {
         return newStudents;
     };
 
+    // Обробник зміни стану перетягування
     const handleDragStateChange = useCallback(() => {
         setDragState(prev => !prev); // Зміна стану перетягування
     }, []);
 
+    // Обробник зміни обраного міста
     const handleCityChange = useCallback((event) => {
         setSelectedCity(event.target.value); // Зміна обраного міста
     }, []);
 
+    // Обробник сортування
     const handleSort = useCallback((direction) => {
         setSortDirection(direction); // Зміна напрямку сортування
     }, []);
 
+    // Обробник очищення сортування та фільтрації
     const handleClear = useCallback(() => {
         setSelectedCity(''); // Очищення обраного міста
         setSortDirection(null); // Скидання напрямку сортування
     }, []);
 
+    // Відфільтровані та відсортовані студенти
     const sortedFilteredStudents = useMemo(() => {
         let filteredStudents = students; // Використання стану для фільтрації студентів
 
@@ -147,6 +187,7 @@ const List = () => {
         return filteredStudents;
     }, [selectedCity, sortDirection, students]); // Додавання студентів до залежностей
 
+    // Перевірка, чи є студенти для відображення
     if (!sortedFilteredStudents.length) {
         return <p>Немає студентів для відображення.</p>;
     }
@@ -154,21 +195,26 @@ const List = () => {
     return (
         <>
             <div className="filters">
+                {/* Фільтр за містом */}
                 <label>
                     Фільтрувати за містом
                     <CityFilter onChange={handleCityChange} />
                 </label>
+                {/* Кнопки для сортування */}
                 <button className="filterButton" onClick={() => handleSort('down')}>
                     Absences <ArrowDownwardIcon />
                 </button>
                 <button className="filterButton" onClick={() => handleSort('up')}>
                     Absences <ArrowUpwardIcon />
                 </button>
+                {/* Кнопка для очищення сортування */}
                 <button className="filterButton" onClick={handleClear}>
                     Clear sorting <HighlightOffIcon />
                 </button>
+                {/* Чекбокс для включення/виключення перетягування */}
                 <Checkbox icon={<ToggleOffIcon />} checkedIcon={<ToggleOnIcon />} onClick={handleDragStateChange} />
             </div>
+            {/* Відображення списку студентів */}
             <div className="users">
                 {sortedFilteredStudents.map(student => (
                     <Student
@@ -187,4 +233,3 @@ const List = () => {
 };
 
 export default List;
-
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,19 +1,21 @@
-import React, {useContext, useEffect, useState} from "react";
-import {auth} from '../../fb-cfg.js';
-import {onAuthStateChanged} from 'firebase/auth';
+import React, { useEffect, useState, useContext } from 'react';
+import { useSelector } from 'react-redux';
+import { auth } from '../../fb-cfg.js';
+import { onAuthStateChanged } from 'firebase/auth';
 import Welcome from "../Welcome.jsx";
 import Field from "../Field.jsx";
 import List from "../List.jsx";
 import "../../main.css"
-import {ThemeContext} from "../ThemeContext.jsx";
-import {useFontSize} from "../Font.jsx";
-import {useFontFamily} from "../FontFamilyContext.jsx";
+import { ThemeContext } from "../ThemeContext.jsx";
+import { useFontSize } from "../Font.jsx";
+import { useFontFamily } from "../FontFamilyContext.jsx";
 
 const Home = () => {
     const { lightMode } = useContext(ThemeContext);
     const { fontSize } = useFontSize();
     const { fontFamily } = useFontFamily();
-    const [ user, setUser ] = useState(null);
+    const [user, setUser] = useState(null);
+    const heartClicks = useSelector(state => state.heartClicks); // Отримання кількості натискань на серце зі стору
 
     useEffect(() => {
         // Очистити підписку, коли компонент знищується
@@ -35,10 +37,11 @@ const Home = () => {
             <Welcome name={"Користувач"} lastname={
                 user ? user.displayName + " " + user.email : "Анонім"}
             />
-            <img width={120} height={120} src={user ? user.photoURL : "https://via.placeholder.com/120"} alt="user"/>
-            <Field label={"Введіть текст"} placeholder={"..."}/>
+            <img width={120} height={120} src={user ? user.photoURL : "https://via.placeholder.com/120"} alt="user" />
+            <Field label={"Введіть текст"} placeholder={"..."} />
             <a href="/pokeapi" className="pokeapi-button">PokeAPI</a>
-            <List/>
+            <p>Кількість натискань на сердечко: {heartClicks}</p> {/* Відображення кількості натискань на серце */}
+            <List />
             {/*<MainContent/>*/}
 
         </div>
